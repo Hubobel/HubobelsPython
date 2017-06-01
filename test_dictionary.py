3c534247581342803edbda0b90ffd7f4ae81b3be
hubobel <carsten@hubobel.de>
2017-06-01 11:41:42 +0200

    läuft in Schleife und pusht SV

--- test_dictionary.py
+++ test_dictionary.py
@@ -1,4 +1,26 @@
-a = {"episode" : "","fanart" : "","file":"","label":"","season":"","showtitle":"","type":"","title":""}
+import urllib.request
+import requests
+def email_alert(first):
+    report = {}
+    report["value1"] = first
 
-for i in a:
-    print(i)
+    urllib.request.urlopen("https://maker.ifttt.com/trigger/kalender/with/key/foLJhy361EqeESdkssI-J", data=report)
+
+
+print("Choose your third string.")
+c = input()
+#email_alert(c)
+url1 = "https://maker.ifttt.com/trigger/iCloud/with/key/foLJhy361EqeESdkssI-J?value1="
+url2 = "&value2=12:05"
+url = url1 + c + url2
+print(url)
+#requests.get("https://maker.ifttt.com/trigger/iCloud/with/key/foLJhy361EqeESdkssI-J?value1=test_final&value2=12:05")
+requests.get(url)
+
+
+
+
+
+
+
+
