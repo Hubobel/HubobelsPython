30d14f989879227c1de5fc52a037171c53f727ab
hubobel <carsten@hubobel.de>
2017-06-03 19:17:41 +0200

    läuft in Schleife und pusht SV

--- test_dictionary.py
+++ test_dictionary.py
@@ -1,26 +1,17 @@
 import urllib.request
 import requests
-def email_alert(first):
-    report = {}
-    report["value1"] = first
+import datetime
 
-    urllib.request.urlopen("https://maker.ifttt.com/trigger/kalender/with/key/foLJhy361EqeESdkssI-J", data=report)
+datum = datetime.datetime.now().strftime("%H:%M")
 
 
 print("Choose your third string.")
 c = input()
 #email_alert(c)
 url1 = "https://maker.ifttt.com/trigger/iCloud/with/key/foLJhy361EqeESdkssI-J?value1="
-url2 = "&value2=12:05"
-url = url1 + c + url2
+url2 = "&value2="
+url = url1 + c + url2 + datum
 print(url)
-#requests.get("https://maker.ifttt.com/trigger/iCloud/with/key/foLJhy361EqeESdkssI-J?value1=test_final&value2=12:05")
+requests.get("https://maker.ifttt.com/trigger/iCloud/with/key/foLJhy361EqeESdkssI-J?value1=test_final&value2=12:05")
 requests.get(url)
-
-
-
-
-
-
-
 
