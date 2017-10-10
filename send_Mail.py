364d649fa2927c0afa01082304d93e00f6293aa4
hubobel <carsten@hubobel.de>
2017-10-10 18:12:09 +0200

    "Pfad" Variable eingefügt

--- send_Mail.py
+++ send_Mail.py
@@ -3,11 +3,14 @@ from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 from email.mime.base import MIMEBase
 from email import encoders
+import os
 
 fromaddr = 'carsten.richter77@gmail.com'
 toaddr = 'carsten.richter77@gmail.com'
 
-fobj = open("mpg/adressen.txt")
+pfad = os.path.dirname(__file__)
+
+fobj = open(pfad+"/mpg/adressen.txt")
 bcc =[]
 for line in fobj:
     a = line.rstrip()
@@ -22,13 +25,12 @@ msg['From'] = fromaddr
 msg['To'] = toaddr
 msg['Subject'] = 'Hello World of Python-LISTENtest'
 
-
 body = 'Hello World again and again and again'
 
 msg.attach(MIMEText(body, 'plain'))
 
 filename = 'heute.pdf'
-attachment = open('mpg/heute.pdf','rb')
+attachment = open(pfad+'/mpg/heute.pdf','rb')
 
 part = MIMEBase('application','octet-stream')
 part.set_payload((attachment).read())
@@ -38,7 +40,7 @@ part.add_header('Content-Disposition','attachment; filename= %s'% filename)
 msg.attach(part)
 
 filename = 'morgen.pdf'
-attachment = open('mpg/morgen.pdf','rb')
+attachment = open(pfad+'/mpg/morgen.pdf','rb')
 
 part = MIMEBase('application','octet-stream')
 part.set_payload((attachment).read())
